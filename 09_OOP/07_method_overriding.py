e70b63d639bf1a14a8c88e78bc30df34bafc76fc
Aditi Rai <rai.aditi2505@gmail.com>
2026-08-11 18:45:54 +0530

    Add method overriding example

---
+++ 09_OOP/07_method_overriding.py
@@ -0,0 +1,17 @@
+# Demonstrate method overriding
+
+class Animal:
+    def sound(self):
+        print("Animal makes a sound.")
+
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks.")
+
+
+animal = Animal()
+dog = Dog()
+
+animal.sound()
+dog.sound()
